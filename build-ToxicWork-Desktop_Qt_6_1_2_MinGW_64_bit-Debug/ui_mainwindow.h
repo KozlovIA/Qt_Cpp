32981b7416474c1b5fedde7b26b779a40aa0ded4
Igorexy <iak7@bk.ru>
2021-07-13 13:30:07 +0300

    Start

---
+++ build-ToxicWork-Desktop_Qt_6_1_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,104 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.1.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QWidget *gridLayoutWidget;
+    QGridLayout *LeftGrid;
+    QTextEdit *textOut;
+    QSpacerItem *verticalSpacer;
+    QTextEdit *textInput;
+    QLabel *label;
+    QPushButton *ResetButton;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(803, 604);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        gridLayoutWidget = new QWidget(centralwidget);
+        gridLayoutWidget->setObjectName(QString::fromUtf8("gridLayoutWidget"));
+        gridLayoutWidget->setGeometry(QRect(0, 90, 801, 471));
+        LeftGrid = new QGridLayout(gridLayoutWidget);
+        LeftGrid->setObjectName(QString::fromUtf8("LeftGrid"));
+        LeftGrid->setContentsMargins(0, 0, 0, 0);
+        textOut = new QTextEdit(gridLayoutWidget);
+        textOut->setObjectName(QString::fromUtf8("textOut"));
+
+        LeftGrid->addWidget(textOut, 0, 2, 1, 1);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        LeftGrid->addItem(verticalSpacer, 0, 1, 1, 1);
+
+        textInput = new QTextEdit(gridLayoutWidget);
+        textInput->setObjectName(QString::fromUtf8("textInput"));
+
+        LeftGrid->addWidget(textInput, 0, 0, 1, 1);
+
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(340, 0, 461, 91));
+        ResetButton = new QPushButton(centralwidget);
+        ResetButton->setObjectName(QString::fromUtf8("ResetButton"));
+        ResetButton->setGeometry(QRect(70, 50, 80, 21));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 803, 20));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+        QObject::connect(ResetButton, &QPushButton::clicked, MainWindow, qOverload<>(&QMainWindow::showNormal));
+        QObject::connect(textInput, &QTextEdit::textChanged, MainWindow, qOverload<>(&QMainWindow::close));
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "\320\222\320\262\320\265\320\264\320\270\321\202\320\265 \321\202\320\265\320\272\321\201\321\202", nullptr));
+        ResetButton->setText(QCoreApplication::translate("MainWindow", "Reset", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
